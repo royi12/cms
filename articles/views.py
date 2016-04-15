925602efaf14af1dd9141950001281fe8d129689
Royi <royi1220@gmail.com>
2016-04-15 22:12:59 +0300

    Changed login and signup forms so that they redirect to path in next GET param.

--- articles/views.py
+++ articles/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.utils import timezone
@@ -58,11 +58,13 @@ class PublishView(View):
 class SignupView(CreateView):
     template_name = 'articles/signup.html'
     form_class = UserCreationForm
-    success_url = reverse_lazy('articles:index')
 
     def form_valid(self, form):
         # Signup and login user
         response = super(SignupView, self).form_valid(form)
-        user = authenticate(username=form.cleaned_data['username'],password=form.cleaned_data['password1'])
+        user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
         login(self.request, user)
         return response
+
+    def get_success_url(self):
+        return self.request.GET.get('next', reverse('articles:index'))
